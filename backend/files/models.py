3538d8e943e6262ade9dae9e8d5b055781487e68
Anup Da <anoopemacs@gmail.com>
2025-08-07 18:22:29 +0530

    Create a File object
    This object represents one of our files

--- backend/files/models.py
+++ backend/files/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    filename = models.CharField(max_length=500, help_text="The name of the file")
+    s3url = models.CharField(max_length=500, help_text="The Amazon S3 url of the file")
+    def __str__(self):
+        return f"{self.filename}"
+    
