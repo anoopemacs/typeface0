a1ecf14099830b772253b836aa029596e1d3254e
Anup Da <anoopemacs@gmail.com>
2025-08-08 08:27:36 +0530

    Instead of Amazon S3, save the File locally on disk, model changes

--- backend/files/models.py
+++ backend/files/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class File(models.Model):
     filename = models.CharField(max_length=500, help_text="The name of the file")
-    s3url = models.CharField(max_length=500, help_text="The Amazon S3 url of the file")
+    diskname = models.CharField(max_length=537, help_text="The on disk uuid name of the file")
     def __str__(self):
-        return f"{self.filename}"
+        return f"{self.diskname}"
     
