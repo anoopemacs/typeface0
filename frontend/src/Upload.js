891404072c168f4da7daf7fa2cb1a7a6430de2e1
Anup Da <anoopemacs@gmail.com>
2025-08-08 19:08:02 +0530

    File upload using axios

--- frontend/src/Upload.js
+++ frontend/src/Upload.js
@@ -1,19 +1,37 @@
+import axios from "axios";
+import React, { useState } from "react";
 import SideBar from './SideBar';
 
-function Upload() {
+const Upload = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const onFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  const onFileUpload = () => {
+    const formData = new FormData();
+    formData.append(
+      "myfile",
+      selectedFile
+    );
+    //console.log(selectedFile);
+    axios
+      .post("http://localhost:8000/files/upload/", formData)
+      .then((response) => {
+        alert(response.data.message);
+      })
+      .catch((err) => alert("Error during file upload"));
+  };
+
   return (
-    <>
+    <div>
       <SideBar />
-      <div className="Upload">
-        <form action="http://localhost:8000/files/upload/"
-              method="post"
-              encType="multipart/form-data">
-          <input type="file" name="myfile" />
-          <input type="submit" name="submit" value="Upload" />
-        </form>
+      <h3>File Uploader</h3>
+      <div>
+	<input type="file" onChange={onFileChange} />
+	<button onClick={onFileUpload}>Upload</button>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Upload;
